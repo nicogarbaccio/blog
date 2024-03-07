904dec998230b792b90997f0b234ceb2bd9aa832
Nico Garbaccio <garbaccio20@gmail.com>
2024-03-06 22:52:33 -0500

    hook up to production and test db

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING)
+  .then(() => {
+    console.log(
+      "connected to database: ",
+      process.env.MONGODB_CONNECTION_STRING
+    );
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
